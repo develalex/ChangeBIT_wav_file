1cc94bf0a8cf060cfee78650b8f876393072953e
develalex <alexander.multon@mail.ru>
2017-06-08 16:31:22 +0300

    changed comments language

--- CursWork_sem3/CursWork_sem3/main.cpp
+++ CursWork_sem3/CursWork_sem3/main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// Структура, описывающая заголовок WAV файла.
+// Strukture WAVE file
 struct WAVEHEADER
 {
 	char chunkId[4];
@@ -21,12 +21,12 @@ struct WAVEHEADER
 	char subchunk2Id[4];
 	unsigned long subchunk2Size;
 };
-bool UP =  false;// true - Увеличиваем или false - уменьшаем битность 
+bool UP =  false;// true - Increase , false - Decrease bit 
 WAVEHEADER header, newheader;
 
 
 void PrintInfo(WAVEHEADER &head){
-	// Метод выводит информацию о файле из заголовка
+	// Print info from fileheader
 	cout << endl << " *** Информация о файле ***" << endl;
 	cout << "Формат: " << head.wFormatTag << endl;
 	cout << "Бит на отсчет: " << head.BitsPerSample << endl;
@@ -47,8 +47,8 @@ int main()
 	string filename;
 	cout << "Введите имя wav файла без расширения  в текущем каталоге: ";
 	cin >> filename;
-	filename.append(".wav");// Добавляем расширение файла
-	//Пробуем открыть файл
+	filename.append(".wav");// Add the file extension
+	//Trying to open the file
 	err = fopen_s(&file, filename.c_str(), "rb");
 	if (err)
 	{
@@ -57,10 +57,10 @@ int main()
 		//delete filename;
 		return 0;
 	}
-	// Читаем заголовок
+	// Read header
 	fread_s(&header, sizeof(WAVEHEADER), sizeof(WAVEHEADER), 1, file);
 
-	// Если формат WAV файла не РСМ, то выдаем сообщение об ошибке и выходим из проги
+	// If format WAV file not PCM, then print errormassage and exit
 	if (header.wFormatTag != 1){
 		PrintInfo(header);
 		cout <<endl<< "Формат файла не 1 PCM. Ничего не сможем сделать." << endl << endl;
@@ -89,18 +89,18 @@ int main()
 	cout << "Исходный файл " << filename << endl;
 	PrintInfo(header);
 	
-		// Читаем оставшуюся часть в массив
+		// Read the rest of the array
 	BYTE* arr = new BYTE[header.chunkSize];
 	fread_s(arr, header.chunkSize, header.chunkSize, 1, file);
 	fclose(file);
 	
-	// *************Пересчитываем заголовок ****************
-	// Количество битов на отсчет ограничено значениями 8-32
-	// Количество сэмплов 
+	// ************* We recalculate the title ****************
+	// The number of bits per count is limited to 8-32
+	// Number of samples
 	const int SampleCount = (header.subchunk2Size * 8 / header.BitsPerSample) / header.nChannels;
 	newheader = header;
 
-	// Если UP true  увеличиваем бит в два раза на отсчет, если false уменьшаем в 2 раза
+	// If UP true, the bit is doubled by counting, if false is reduced by 2 times
 	newheader.BitsPerSample = UP ? (header.BitsPerSample * 2) : (header.BitsPerSample / 2);
 	if (newheader.BitsPerSample < 8){
 		newheader.BitsPerSample = 8;
@@ -110,14 +110,14 @@ int main()
 		newheader.BitsPerSample = 32;
 		cout << "Max BitPerSample is 32" << endl;
 	}
-	//ByteAlign может принимать значения 1,2,4  ***** 
-	//Высчитываем нужное кол-во байт
+	// ByteAlign can take the values 1,2,4 *****
+	// Calculate the required number of bytes
 	newheader.blockAlign = (newheader.BitsPerSample / 8)*header.nChannels;
 
-	// Если кол-во бит не влезает в переменную то увеличиваем кол-во байт на 1
+	// If the number of bits does not fit into the variable, then increase the number of bytes by 1
 	if (newheader.blockAlign<(newheader.BitsPerSample / 8))  newheader.blockAlign++;
 
-	// Проверяем значение ByteAlign должно быть 1,2 или 4 или 8
+	// Check the value of ByteAlign should be 1,2 or 4 or 8
 	if ((newheader.blockAlign != 1) && (newheader.blockAlign != 2) &&
 		(newheader.blockAlign != 4) && (newheader.blockAlign != 8))
 		cout << "Ошибка ByteAlign : " << newheader.blockAlign << endl;
@@ -125,14 +125,14 @@ int main()
 	newheader.AvgBytesPerSec = newheader.SamplesPerSec*newheader.blockAlign;
 	newheader.subchunk2Size = newheader.blockAlign*SampleCount;// !!!!!!!!
 	newheader.chunkSize = newheader.subchunk2Size + sizeof(header) - 8;
-	// ***********Конец пересчета заголовка**************
+	// *********** End of header recalculation **************
 
-		// Создаем новое имя файла. добавляем (converted) перед расширением файла
+	// Create a new file name. add (converted) before the file extension
 	string newfilename = filename;
-	newfilename.resize(filename.size() - 4); // Отрубаем расширение файла
+	newfilename.resize(filename.size() - 4); 
 	newfilename.append("(").append(std::to_string(newheader.BitsPerSample)).append("bit)").append(".wav");
 	
-	// Пробуем создать файл
+	// Try to file create
 	err = fopen_s(&file,newfilename.c_str(), "w+b");
 	if (err)
 	{
@@ -143,7 +143,7 @@ int main()
 	
 	cout << endl << "Пробуем конвертировать c " << header.BitsPerSample << " бит в "
 		<< newheader.BitsPerSample << " бит." << endl;
-	//записываем в новый файл новый header
+	//write new header to file
 	fwrite(&newheader,1, sizeof(newheader), file);
 
 	int block = (newheader.blockAlign - header.blockAlign)/header.nChannels;
@@ -153,60 +153,60 @@ int main()
 		fclose(file);
 	}
 	else{
-		int newarrCount = 0;// Итератор для нового массива данных
-		int arrCount = 0; // Итератор исходного массива данных
-		int needCopyByte = (abs(block) );;// Кол-во байт необходимых для копирования 
-		// Задаем размер нового блока данных
+		int newarrCount = 0;// Iterator of new array
+		int arrCount = 0; // Iterator of source data array
+		int needCopyByte = (abs(block) );;// Number of bytes required for copying
+// Set the size of the new data block
 		BYTE* newarr = new BYTE[newheader.subchunk2Size];
 
-		// Проходим по исходному блоку данных
+		
 		for (unsigned int itSample = 0; itSample < (header.subchunk2Size / header.blockAlign); itSample++){
 			for (int itChannel = 0; itChannel < header.nChannels; itChannel++){
 
-				// Если новый размер блока больше исходного, то добавляем нулевые байты
+				// If the new block size is larger, then add zero bytes
 				if (block > 0){
 					for (int i = 0; i < block; i++){
 						newarr[newarrCount] = 0;
 						newarrCount++;
 					}
 				}
-				// Если новый размер данных меньше то пропускаем старшие байты
+				// If the new data size is less then skip the high-order bytes
 				if (block < 0){
 					for (int i = 0; i > block; i--){
 						arrCount++;
 					}
 				}
 
-				// **************  Копирование в новый массив  **************
-				// Прогуливаемся по байтам одного канала и копируем их в новый массив
+				// ************** Copying to a new array **************
+				// Walk through the bytes of one channel and copy them into a new array
 				for (int itByte = 0; itByte < needCopyByte; itByte++){
 					newarr[newarrCount] = arr[arrCount];
 					
-					//*************************************************
-					//Если исходная инфа более 8 бит, то при переходе к 8 битам и менее
-					//Необходимо прибавить 128. Т.к. в например в 16 битах 
-					//значения идет со знаком (от -32768 до +32767). а в 8 битах
-					// без знака (от 0 до 255 )
+					// ************************************************ *
+					// If the source info is more than 8 bits, then when changing to 8 bits or less
+					// You need to add 128. In for example 16 bits
+					// value goes with a sign (-32768 to +32767). But in 8 bits
+					// without sign (from 0 to 255)
 					if ((header.BitsPerSample>8) && (newheader.BitsPerSample <= 8)){
 						newarr[newarrCount] = (BYTE)(newarr[newarrCount] + 128);
 					}
-					//Если конвертируем с 8 бит на большее, то надо отнять 128
+					// If you convert from 8 bits to more, you need to take 128
 					if ((header.BitsPerSample <= 8) && (newheader.BitsPerSample > 8)){
 						newarr[newarrCount] = (BYTE)(newarr[newarrCount] - 128);
 					}
 					//*************************************************
 					newarrCount++;
 					arrCount++;
-				}// Конец itByte
-			}// Конец itChannel
-		}// Конец  itSample
+				}// End of itByte
+			}// End of itChannel
+		}// End of itSample
 
 		fwrite(newarr,1, newheader.chunkSize, file);
-		fclose(file);// Закрываем файл
-		delete[]newarr;// Освобождаем память
-	} //Конец if (block == 0) else
+		fclose(file);// Close file
+		delete[]newarr;// memory free
+	} 
 
-	delete[]arr;// Освобождаем память
+	delete[]arr;// memory free
 	cout << endl << "Файл " << filename << " конвертирован. Создан новый файл " << newfilename << endl;
 	cout << " ***** Параметры нового файла *****" << endl;
 	PrintInfo(newheader);
